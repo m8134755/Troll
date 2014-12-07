17ff839b86e28512614b9be9e3a904ee49c50fb8
DoHyung <kdh7785@naver.com>
2014-12-07 23:44:48 +0900

    불필요코드 삭제

--- WebContent/js/script_global.js
+++ WebContent/js/script_global.js
@@ -1,19 +1,6 @@
 // JavaScript source code
 
-function setComma(str)
-{
-        str = ""+str+"";
-        var retValue = "";
-        for(var i=0; i<str.length; i++)
-        {
-                if(i > 0 && (i%3)==0) {
-                        retValue = str.charAt(str.length - i -1) + "," + retValue;
-                 } else {
-                        retValue = str.charAt(str.length - i -1) + retValue;
-                }
-        }
-        return retValue +"원";
-}
+
 
 function setProgress(string){
 	$('<div class="loadingdiv fade"><div class="loading-container container hidden-xs"><div class="loading fade in"></div><img class="fade success" src="/images/success.png"/><img class="fade fail" src="/images/fail.png"/><h2 class="fade in text-right">' + string + '</h2></div>' +
@@ -83,14 +70,6 @@ function setSuccess(string){
 			$('div.loadingdiv').remove();
 		}, 300);
 	});
-}
-
-function pushNoti(name, msg){
-	setProgress('푸시 알림을 요청하고 있습니다..');
-	$.post('/push.jsp', {username:name.toString(), msg: msg}, function(data){
-		if(data.status == 1) setSuccess('푸시 알림이 정상적으로 요청되었습니다.');
-		else setError('푸시 알림을 요청하는 중 에러가 발생했습니다.');
-	}, 'json');
 }
 
 setTimeout(function(){
